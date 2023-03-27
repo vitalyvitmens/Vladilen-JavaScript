30a7c5df0b6c7464ddbb072b632048167925ca73
vitalyvitmens <otm_@tut.by>
2023-03-27 13:36:46 +0300

    first commit

---
+++ Module1_Basic_JavaScript/first-project/js-info.js
@@ -0,0 +1,5 @@
+alert('Брендан Эйх')
+alert('За 10 дней')
+alert('1995')
+alert('JavaScript - это не Java')
+alert('ECMAScript')
---
+++ Module1_Basic_JavaScript/index.js
@@ -0,0 +1,8 @@
+console.log('Hello World 1')
+console.log('Hello World 2')
+console.log('Hello World 3')
+
+alert('Hello World!')
+alert('123')
+
+prompt('enter your name', 'Hello')
