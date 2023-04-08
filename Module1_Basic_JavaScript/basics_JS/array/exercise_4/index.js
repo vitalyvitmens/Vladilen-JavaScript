f1cf09066f6bdf37e4068ca4448ea66b81de11c0
vitalyvitmens <otm_@tut.by>
2023-04-08 13:35:56 +0300

    add data in project

--- Module1_Basic_JavaScript/basics_JS/array/exercise_4/index.js
+++ Module1_Basic_JavaScript/basics_JS/array/exercise_4/index.js
@@ -2,6 +2,6 @@ const coffees = ['Latte', 'Cappuccino', 'Americano']
 const prices = [1.5, 1, 2]
 
 const updatedPrices = prices.map((price) => price + 0.5)
-coffees.forEach((coffee, index) =>
-  alert(`Кофе ${coffee} сейчас стоит ${updatedPrices[index]} евро`)
+coffees.forEach((name, index) =>
+  alert(`Кофе ${name} сейчас стоит ${updatedPrices[index]} евро`)
 )
