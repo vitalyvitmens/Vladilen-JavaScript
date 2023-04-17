70e67f6f878c29f9b784a7d8c71dff479d955ba8
vitalyvitmens <otm_@tut.by>
2023-04-17 16:25:40 +0300

    add data in project

---
+++ Module1_Basic_JavaScript/basics_JS/DOM/index.js
@@ -0,0 +1,10 @@
+const tasksBlock = document.getElementById('tasks')
+console.log(tasks)
+
+const allNavButtons = document.getElementsByClassName(
+  'main-navigation__button-item'
+)
+console.log(allNavButtons)
+
+const allButtons = document.getElementsByTagName('button')
+console.log(allButtons)
