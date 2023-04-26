877a1506217cdce5acc58e7adfa37df75d9cd39f
vitalyvitmens <otm_@tut.by>
2023-04-26 12:33:03 +0300

    add data in project

--- Module1_Basic_JavaScript/basics_JS/Async + Await/exercise_2/index.js
+++ Module1_Basic_JavaScript/basics_JS/Async + Await/exercise_2/index.js
@@ -7,7 +7,7 @@ let getAllPromise = async (requests) => {
 
 const getTodosByIds = async (ids) => {
   try {
-    const requests = await ids.map((id) => fetch(`${TODOS_URL}/${id}`))
+    const requests = ids.map((id) => fetch(`${TODOS_URL}/${id}`))
     const allPromiseRequests = await getAllPromise(requests)
     const dataResults = allPromiseRequests.map((data) => data.json())
     const allTasks = await getAllPromise(dataResults)
@@ -25,9 +25,7 @@ const getTodosByIds2 = async (ids) => {
     console.log(
       await getAllPromise(
         (
-          await getAllPromise(
-            await ids.map((id) => fetch(`${TODOS_URL}/${id}`))
-          )
+          await getAllPromise(ids.map((id) => fetch(`${TODOS_URL}/${id}`)))
         ).map((data) => data.json())
       )
     )
@@ -37,3 +35,34 @@ const getTodosByIds2 = async (ids) => {
 }
 
 getTodosByIds2([43, 21, 55, 100, 10])
+
+//! Решение от Result School
+// const getTodosByIds = (ids) => {
+//     const requests = ids.map((id) => fetch(`${TODOS_URL}/${id}`));
+//     Promise.all(requests)
+//         .then((responses) => {
+//             const dataResults = responses.map((data) => data.json());
+//             return Promise.all(dataResults)
+//         })
+//         .then((allTasks) => {
+//             console.log(allTasks);
+//         })
+//         .catch((error) => {
+//             console.log(error);
+//         })
+// }
+
+// getTodosByIds([43, 21, 55, 100, 10]);
+
+const getTodosByIds3 = async (ids) => {
+  try {
+    const requests = ids.map((id) => fetch(`${TODOS_URL}/${id}`));
+    const responses = await Promise.all(requests);
+    const allTasks = await Promise.all(responses.map((data) => data.json()));
+    console.log("allTasks", allTasks);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+getTodosByIds3([43, 21, 55, 100, 10]);
