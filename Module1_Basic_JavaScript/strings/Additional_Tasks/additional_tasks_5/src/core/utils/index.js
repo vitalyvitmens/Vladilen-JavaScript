9addd7951bc637964b48b13c6b0ac618729b84fc
vitalyvitmens <otm_@tut.by>
2023-04-29 19:43:24 +0300

    add data in project

---
+++ Module1_Basic_JavaScript/strings/Additional_Tasks/additional_tasks_5/src/core/utils/index.js
@@ -0,0 +1,8 @@
+import moment from 'moment'
+
+export const calculateSumOfNumbers = (numbers) =>
+  numbers.reduce((acc, cur) => acc += cur, 0)
+
+export const getFormattedTime = (date) => {
+  return moment(date).format('MMMM Do YYYY, h:mm:ss a')
+}
