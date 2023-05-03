a4250147c6fe00ed623b01c4a5ba099698760906
vitalyvitmens <otm_@tut.by>
2023-05-03 12:32:23 +0300

    add data in branch main

--- Module1_Basic_JavaScript/Git/index.js
+++ Module1_Basic_JavaScript/Git/index.js
@@ -1,3 +1,10 @@
 console.log(`Мы находимся на ветке main`)
 
 console.log(`Удали этот консоль лог при пул реквесте`)
+
+const userAge = 35
+if (userAge >= 18) {
+  console.log(`Вы совершеннолетний`)
+} else {
+  console.log(`Вы НЕ совершеннолетний`)
+}
